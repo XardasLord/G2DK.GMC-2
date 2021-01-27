63076ba8b2612cbe9ef82677a4cc1721c18975f0
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-01-27 15:26:00 +0100

    Added GMC-2 init project

---
+++ GothicModComposer/Launcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GothicModComposer
+{
+    internal class Launcher
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
