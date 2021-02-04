21f6c2640671ff504806893bf86a29b469fb2d1c
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-02-04 10:23:56 +0100

    Add default world command & undo logic

--- GothicModComposer/Commands/AddDefaultWorldCommand.cs
+++ GothicModComposer/Commands/AddDefaultWorldCommand.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using GothicModComposer.Commands.ExecutedCommandActions.Interfaces;
-using GothicModComposer.Models.Profiles;
+﻿using GothicModComposer.Models.Profiles;
 using GothicModComposer.Utils;
 
 namespace GothicModComposer.Commands
@@ -11,7 +8,6 @@ namespace GothicModComposer.Commands
 		public string CommandName => "Add default world";
 
 		private readonly IProfile _profile;
-		private static readonly Stack<ICommandActionIO> ExecutedActions = new();
 
 		public AddDefaultWorldCommand(IProfile profile )
 			=> _profile = profile;
@@ -24,23 +20,16 @@ namespace GothicModComposer.Commands
 				return;
 			}
 
-			_profile.GothicArguments._3D(_profile.DefaultWorld);
-			Logger.Info($"Added --3D:{_profile.DefaultWorld} to Gothic arguments.");
+			AddDefaultWorld();
 		}
 
-		public void Undo()
-		{
-			if (!ExecutedActions.Any())
-			{
-				Logger.Info("There is nothing to undo, because no actions were executed.");
-				return;
-			}
+		public void Undo() 
+			=> Logger.Info("No need to undo this action since it didn't execute anything related to the Gothic system.");
 
-			while (ExecutedActions.Count > 0)
-			{
-				var executedAction = ExecutedActions.Pop();
-				executedAction?.Undo();
-			}
+		private void AddDefaultWorld()
+		{
+			_profile.GothicArguments._3D(_profile.DefaultWorld);
+			Logger.Info($"Added --3D:{_profile.DefaultWorld} to Gothic arguments.");
 		}
 	}
 }
