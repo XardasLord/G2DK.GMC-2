9910eb5bb29e1445cc7c04c2675514e92aa1e549
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-04-02 12:42:38 +0200

    Very basic UI layout without logic

---
+++ GothicModComposer.UI.ViewModels/Commands/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace GothicModComposer.UI.ViewModels.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            if (execute is null)
+                throw new ArgumentNullException(nameof(execute), "Command to execute is null");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+            => _canExecute is null || _canExecute(parameter);
+
+        public void Execute(object parameter)
+            => _execute(parameter);
+
+        public void RaiseCanExecuteChanged()
+            => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ GothicModComposer.UI.ViewModels/ViewModels/GmcVM.cs
@@ -0,0 +1,49 @@
+﻿using GothicModComposer.UI.ViewModels.Commands;
+
+namespace GothicModComposer.UI.ViewModels.ViewModels
+{
+    public class GmcVM : Observable
+    {
+        public RelayCommand RunUpdateProfile { get; }
+        public RelayCommand RunComposeProfile { get; }
+        public RelayCommand RunDKProfile { get; }
+        public RelayCommand RunRestoreGothicProfile { get; }
+        public RelayCommand RunBuildModFileProfile { get; }
+        public RelayCommand RunEnableVDFProfile { get; }
+
+        public GmcVM()
+        {
+            RunUpdateProfile = new RelayCommand(RunUpdateProfileExecute, x => true);
+            RunComposeProfile = new RelayCommand(RunComposeProfileExecute, x => true);
+            RunDKProfile = new RelayCommand(RunDKProfileExecute, x => true);
+            RunRestoreGothicProfile = new RelayCommand(RunRestoreGothicProfileExecute, x => true);
+            RunBuildModFileProfile = new RelayCommand(RunBuildModFileProfileProfileExecute, x => true);
+            RunEnableVDFProfile = new RelayCommand(RunEnableVDFProfileProfileExecute, x => true);
+        }
+
+
+        private void RunUpdateProfileExecute(object obj)
+        {
+        }
+
+        private void RunComposeProfileExecute(object obj)
+        {
+        }
+
+        private void RunDKProfileExecute(object obj)
+        {
+        }
+
+        private void RunRestoreGothicProfileExecute(object obj)
+        {
+        }
+
+        private void RunBuildModFileProfileProfileExecute(object obj)
+        {
+        }
+
+        private void RunEnableVDFProfileProfileExecute(object obj)
+        {
+        }
+    }
+}
