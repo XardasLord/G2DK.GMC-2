78ff3abd33221562fc3db77d3992149785945488
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-04-10 10:51:51 +0200

    Add two more unit tests for create backup command logic

--- tests/GothicModComposer.UnitTests/Commands/CreateBackupCommandTests.cs
+++ tests/GothicModComposer.UnitTests/Commands/CreateBackupCommandTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using GothicModComposer.Commands;
 using GothicModComposer.Models.Profiles;
 using GothicModComposer.Presets;
@@ -70,7 +72,6 @@ namespace GothicModComposer.UnitTests.Commands
             _fileSystemMock.Setup(x => x.Path.Combine(gothicWorkDataFolderPath, It.IsAny<string>())).Returns(assetSourceDirectoryPath);
             _fileSystemMock.Setup(x => x.Path.Combine(gmcBackupWorkDataFolderPath, It.IsAny<string>())).Returns(assetDestinationDirectoryPath);
             _fileSystemMock.Setup(x => x.Directory.Exists(assetSourceDirectoryPath)).Returns(false);
-            _fileSystemMock.Setup(x => x.Directory.Exists(modExtensionsFolderPath)).Returns(false);
 
             Act();
 
@@ -93,12 +94,89 @@ namespace GothicModComposer.UnitTests.Commands
             _fileSystemMock.Setup(x => x.Path.Combine(gothicWorkDataFolderPath, It.IsAny<string>())).Returns(assetSourceDirectoryPath);
             _fileSystemMock.Setup(x => x.Path.Combine(gmcBackupWorkDataFolderPath, It.IsAny<string>())).Returns(assetDestinationDirectoryPath);
             _fileSystemMock.Setup(x => x.Directory.Exists(assetSourceDirectoryPath)).Returns(true);
-            _fileSystemMock.Setup(x => x.Directory.Exists(modExtensionsFolderPath)).Returns(false);
 
             Act();
             
             var numberOfAssets = Enum.GetNames(typeof(AssetPresetType)).Length;
             _fileSystemMock.Verify(x => x.Directory.Move(assetSourceDirectoryPath, assetDestinationDirectoryPath), Times.Exactly(numberOfAssets));
+        }
+        
+
+        [Fact]
+        public void Execute_WhenModExtensionDirectoryNotExist_ShouldNotMoveToBackup()
+        {
+            const string gothicWorkDataFolderPath = "C:/WorkDataFolderPath";
+            const string modExtensionsFolderPath = "C:/ExtensionsFolderPath";
+            const string gmcBackupWorkDataFolderPath = "C:/BackupWorkDataFolderPath";
+            const string assetSourceDirectoryPath = "SourcePath";
+            const string assetDestinationDirectoryPath = "DestinationPath";
+
+            _profileMock.SetupGet(x => x.GmcFolder.DoesBackupFolderExist).Returns(false);
+            _profileMock.SetupGet(x => x.GmcFolder.BackupWorkDataFolderPath).Returns(gmcBackupWorkDataFolderPath);
+            _profileMock.SetupGet(x => x.GothicFolder.WorkDataFolderPath).Returns(gothicWorkDataFolderPath);
+            _profileMock.SetupGet(x => x.ModFolder.ExtensionsFolderPath).Returns(modExtensionsFolderPath);
+            _fileSystemMock.Setup(x => x.Path.Combine(gothicWorkDataFolderPath, It.IsAny<string>())).Returns(assetSourceDirectoryPath);
+            _fileSystemMock.Setup(x => x.Path.Combine(gmcBackupWorkDataFolderPath, It.IsAny<string>())).Returns(assetDestinationDirectoryPath);
+            _fileSystemMock.Setup(x => x.Directory.Exists(assetSourceDirectoryPath)).Returns(false);
+            _fileSystemMock.Setup(x => x.Directory.Exists(modExtensionsFolderPath)).Returns(false);
+
+            Act();
+
+            _fileSystemMock.Verify(x => x.Directory.GetAllFilesInDirectory(modExtensionsFolderPath, SearchOption.AllDirectories), Times.Never);
+            _fileSystemMock.Verify(x => x.Directory.CreateIfNotExist(It.IsAny<string>()), Times.Never);
+            _fileSystemMock.Verify(x => x.File.Copy(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+        }
+
+
+        [Fact]
+        public void Execute_WhenModExtensionDirectoryExists_ShouldMoveToBackup()
+        {
+            const string gothicWorkDataFolderPath = "C:/WorkDataFolderPath";
+            const string modExtensionsFolderPath = "C:/ExtensionsFolderPath";
+            const string gmcBackupWorkDataFolderPath = "C:/BackupWorkDataFolderPath";
+            const string assetSourceDirectoryPath = "SourcePath";
+            const string assetDestinationDirectoryPath = "DestinationPath";
+            const string gothicBasePath = "C:/BasePath";
+            const string gmcBackupFolderPath = "C:/BackupFolderPath";
+
+            _profileMock.SetupGet(x => x.GmcFolder.DoesBackupFolderExist).Returns(false);
+            _profileMock.SetupGet(x => x.GmcFolder.BackupWorkDataFolderPath).Returns(gmcBackupWorkDataFolderPath);
+            _profileMock.SetupGet(x => x.GothicFolder.WorkDataFolderPath).Returns(gothicWorkDataFolderPath);
+            _profileMock.SetupGet(x => x.ModFolder.ExtensionsFolderPath).Returns(modExtensionsFolderPath);
+            _profileMock.SetupGet(x => x.GothicFolder.BasePath).Returns(gothicBasePath);
+            _profileMock.SetupGet(x => x.GmcFolder.BackupFolderPath).Returns(gmcBackupFolderPath);
+            _fileSystemMock.Setup(x => x.Path.Combine(gothicWorkDataFolderPath, It.IsAny<string>())).Returns(assetSourceDirectoryPath);
+            _fileSystemMock.Setup(x => x.Path.Combine(gmcBackupWorkDataFolderPath, It.IsAny<string>())).Returns(assetDestinationDirectoryPath);
+            _fileSystemMock.Setup(x => x.Directory.Exists(assetSourceDirectoryPath)).Returns(false);
+
+            _fileSystemMock.Setup(x => x.Directory.Exists(modExtensionsFolderPath)).Returns(true);
+            _fileSystemMock
+                .Setup(x => x.Directory.GetAllFilesInDirectory(modExtensionsFolderPath, SearchOption.AllDirectories))
+                .Returns(new List<string> { "File1", "File2" });
+            _fileSystemMock
+                .Setup(x => x.Path.GetRelativePath(modExtensionsFolderPath, It.IsAny<string>()))
+                .Returns("RelativePath");
+            _fileSystemMock
+                .Setup(x => x.Path.Combine(gothicBasePath, "RelativePath"))
+                .Returns("ExtensionFileGothicPath");
+            _fileSystemMock
+                .Setup(x => x.Path.Combine(gmcBackupFolderPath, "RelativePath"))
+                .Returns("ExtensionFileGmcBackupPath");
+            _fileSystemMock
+                .Setup(x => x.File.Exists("ExtensionFileGothicPath"))
+                .Returns(true);
+            _fileSystemMock
+                .Setup(x => x.Path.GetDirectoryName("ExtensionFileGmcBackupPath"))
+                .Returns("Dir");
+            _fileSystemMock
+                .Setup(x => x.Directory.Exists("Dir"))
+                .Returns(false);
+
+            Act();
+
+            _fileSystemMock.Verify(x => x.Directory.GetAllFilesInDirectory(modExtensionsFolderPath, SearchOption.AllDirectories), Times.Once);
+            _fileSystemMock.Verify(x => x.Directory.CreateIfNotExist(It.IsAny<string>()), Times.Exactly(2));
+            _fileSystemMock.Verify(x => x.File.Copy(It.IsAny<string>(), It.IsAny<string>()), Times.Exactly(2));
         }
     }
 }
