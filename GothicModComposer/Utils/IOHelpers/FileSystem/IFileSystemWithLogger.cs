6c6796ab43342c8be2de8ac0b9d83a2f8e9ce86c
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-04-10 09:11:27 +0200

    Remove unused IO objects from IFileSystem abstraction

--- GothicModComposer/Utils/IOHelpers/FileSystem/IFileSystemWithLogger.cs
+++ GothicModComposer/Utils/IOHelpers/FileSystem/IFileSystemWithLogger.cs
@@ -1,6 +1,4 @@
-﻿using System.IO.Abstractions;
-
-namespace GothicModComposer.Utils.IOHelpers.FileSystem
+﻿namespace GothicModComposer.Utils.IOHelpers.FileSystem
 {
     public interface IFileSystemWithLogger
     {
@@ -8,16 +6,6 @@ namespace GothicModComposer.Utils.IOHelpers.FileSystem
 
         IDirectoryWithLogger Directory { get; }
 
-        IFileInfoFactory FileInfo { get; }
-
-        IFileStreamFactory FileStream { get; }
-
         IPathWithLogger Path { get; }
-
-        IDirectoryInfoFactory DirectoryInfo { get; }
-
-        IDriveInfoFactory DriveInfo { get; }
-
-        IFileSystemWatcherFactory FileSystemWatcher { get; }
     }
 }
