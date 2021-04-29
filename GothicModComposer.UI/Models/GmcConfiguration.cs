38f7e8e06c6126b39c6553d4fa096de1cac1bfd4
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-04-29 08:09:32 +0200

    Create default configuration if not exist.

--- GothicModComposer.UI/Models/GmcConfiguration.cs
+++ GothicModComposer.UI/Models/GmcConfiguration.cs
@@ -1,20 +1,56 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json;
 
 namespace GothicModComposer.UI.Models
 {
     public class GmcConfiguration
     {
-        public string DefaultWorld { get; }
-        public List<string> IniOverrides { get; }
-        public GothicVdfsConfig GothicVdfsConfig { get; }
+        public string DefaultWorld { get; set; }
+        public List<string> IniOverrides { get; set; }
+        public GothicVdfsConfig GothicVdfsConfig { get; set; }
 
         public static GmcConfiguration CreateDefault()
         {
-            //TODO: Create default json configuration file under this path
-            return new GmcConfiguration();
+            const string defaultConfig = @"
+{
+	""DefaultWorld"": ""VADUZWORLD.ZEN"",
+
+    ""IniOverrides"": 
+    [
+
+        ""playLogoVideos = 0"",
+        ""subTitles = 1""
+    ],
+    ""GothicVdfsConfig"": 
+    {
+        ""Filename"": ""DK.mod"",
+        ""Comment"": ""Gothic II - The History of Khorinis (%%D)%%N(C) 2021 SoulFire"",
+        ""Directories"": 
+        [
+            ""_work\\Data\\Anims\\_compiled"",
+            ""_work\\Data\\Meshes\\_compiled"",
+            ""_work\\Data\\Textures\\_compiled"",
+            ""_work\\Data\\Scripts\\_compiled"",
+            ""_work\\Data\\Scripts\\Content\\Cutscene\\OU.bin"",
+            ""_work\\Data\\Sound\\Speech"",
+            ""_work\\Data\\Sound\\SFX"",
+            ""_work\\Data\\Worlds""
+        ],
+        ""Include"": 
+        [
+        ],
+        ""Exclude"": 
+        [
+            ""_work\\Data\\Worlds\\DK_SUBZENS\\*"",
+            ""_work\\Data\\Worlds\\VOBTREE\\*"",
+            ""_work\\Data\\Scripts\\Content\\Cutscene\\OU.csl""
+        ]
+    }
+}";
+            return JsonSerializer.Deserialize<GmcConfiguration>(defaultConfig);
         }
 
-        private GmcConfiguration()
+        public GmcConfiguration()
         {
         }
     }
