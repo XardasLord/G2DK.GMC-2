22a3b636ee87633c4d7be0699ecb5d4503fbdeee
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-06-07 21:42:29 +0200

    Managing Overrides Ini works from GUI

---
+++ GothicModComposer.UI/Models/IniOverride.cs
@@ -0,0 +1,22 @@
+﻿using GothicModComposer.UI.Helpers;
+
+namespace GothicModComposer.UI.Models
+{
+    public class IniOverride : ObservableVM
+    {
+        private string _key;
+        private string _value;
+
+        public string Key
+        {
+            get => _key;
+            set => SetProperty(ref _key, value);
+        }
+
+        public string Value
+        {
+            get => _value;
+            set => SetProperty(ref _value, value);
+        }
+    }
+}
