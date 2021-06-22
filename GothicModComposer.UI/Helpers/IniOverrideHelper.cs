f1e52c31d2c5023b26fe3e4e69532cc022f70c9a
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-06-22 22:11:02 +0200

    Added 'Section' value into IniOverride for GUI purposes.

--- GothicModComposer.UI/Helpers/IniOverrideHelper.cs
+++ GothicModComposer.UI/Helpers/IniOverrideHelper.cs
@@ -8,9 +8,9 @@ namespace GothicModComposer.UI.Helpers
         public static List<IniOverride> DefaultIniOverrideKeys
             => new()
             {
-                new IniOverride { Key = "recalc", Value = "0" },
-                new IniOverride { Key = "sightValue", Value = "14" },
-                new IniOverride { Key = "modelDetail", Value = "1" },
+                new IniOverride { Key = "recalc", Value = "0", Section = "[PERFORMANCE]" },
+                new IniOverride { Key = "sightValue", Value = "14", Section = "[PERFORMANCE]" },
+                new IniOverride { Key = "modelDetail", Value = "1", Section = "[PERFORMANCE]" },
             };
     }
 }
