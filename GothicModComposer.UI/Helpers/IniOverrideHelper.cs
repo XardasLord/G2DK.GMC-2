21b68295dc9f1ed04dd04f29e04cdfa277cbf862
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-06-22 18:45:18 +0200

    Added Ini Override Helper for UI default values.

---
+++ GothicModComposer.UI/Helpers/IniOverrideHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using GothicModComposer.UI.Models;
+
+namespace GothicModComposer.UI.Helpers
+{
+    public static class IniOverrideHelper
+    {
+        public static List<IniOverride> DefaultIniOverrideKeys
+            => new()
+            {
+                new IniOverride { Key = "recalc", Value = "0" },
+                new IniOverride { Key = "sightValue", Value = "14" },
+                new IniOverride { Key = "modelDetail", Value = "1" },
+            };
+    }
+}
