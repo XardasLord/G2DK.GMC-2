f48bcbe7c95941612b99e9177433d2de354adf15
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-06-28 08:50:08 +0200

    Added DynamicDataTemplateSelector to get customizable datagrid column type.

---
+++ GothicModComposer.UI/Helpers/DynamicDataTemplateSelector.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using GothicModComposer.UI.Models;
+
+namespace GothicModComposer.UI.Helpers
+{
+    public class DynamicDataTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var element = container as FrameworkElement;
+
+            if (element != null && item != null)
+            {
+                var model = item as IniOverride;
+
+                return (DataTemplate)element.FindResource(model.DisplayAs + "Template");
+            }
+
+            return null;
+        }
+    }
+}
