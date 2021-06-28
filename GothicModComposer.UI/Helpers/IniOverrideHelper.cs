84a434168432ce54ce548b21daf992ec40695fe1
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-06-28 17:13:39 +0200

    Adjusted existing Override ini values to have specific column type and optional available values.

--- GothicModComposer.UI/Helpers/IniOverrideHelper.cs
+++ GothicModComposer.UI/Helpers/IniOverrideHelper.cs
@@ -5,12 +5,16 @@ namespace GothicModComposer.UI.Helpers
 {
     public static class IniOverrideHelper
     {
+        private const string TextBoxColumnName = "TextBox";
+        private const string CheckBoxColumnName = "CheckBox";
+        private const string ComboBoxColumnName = "ComboBox";
+        
         public static List<IniOverride> DefaultIniOverrideKeys
             => new()
             {
-                new IniOverride { Key = "recalc", Value = "0", Section = "[PERFORMANCE]" },
-                new IniOverride { Key = "sightValue", Value = "14", Section = "[PERFORMANCE]" },
-                new IniOverride { Key = "modelDetail", Value = "1", Section = "[PERFORMANCE]" },
+                new IniOverride { Key = "recalc", Value = "0", Section = "[PERFORMANCE]", DisplayAs = ComboBoxColumnName, AvailableValues = new List<string> { "0", "1" } },
+                new IniOverride { Key = "sightValue", Value = "14", Section = "[PERFORMANCE]", DisplayAs = TextBoxColumnName },
+                new IniOverride { Key = "modelDetail", Value = "1", Section = "[PERFORMANCE]", DisplayAs = TextBoxColumnName },
             };
     }
 }
