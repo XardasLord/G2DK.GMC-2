7a40a07c2360d7039c4999870b4c52d944844e0d
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-07-13 08:31:22 +0200

    Added Zen3DWorld model.

---
+++ GothicModComposer.UI/Models/Zen3DWorld.cs
@@ -0,0 +1,16 @@
+﻿namespace GothicModComposer.UI.Models
+{
+    public class Zen3DWorld
+    {
+        public string Path { get; }
+        public string Name { get; }
+
+        public Zen3DWorld(string fullPath, string name)
+        {
+            var indexofWorldSubPath = fullPath.IndexOf("Worlds\\");
+            
+            Path = fullPath.Remove(0, indexofWorldSubPath + 7);
+            Name = name;
+        }
+    }
+}
