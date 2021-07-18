dd6abc3dd0d616fc0322a636fb55399c73b596eb
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-07-18 10:25:12 +0200

    Replaced const strings with enum to define column type to display for IniOverrides.

--- GothicModComposer.UI/Helpers/IniOverrideHelper.cs
+++ GothicModComposer.UI/Helpers/IniOverrideHelper.cs
@@ -5,16 +5,12 @@ namespace GothicModComposer.UI.Helpers
 {
     public static class IniOverrideHelper
     {
-        private const string TextBoxColumnName = "TextBox";
-        private const string CheckBoxColumnName = "CheckBox";
-        private const string ComboBoxColumnName = "ComboBox";
-        
         public static List<IniOverride> DefaultIniOverrideKeys
             => new()
             {
-                new IniOverride { Key = "recalc", Value = "0", Section = "PERFORMANCE", DisplayAs = ComboBoxColumnName, AvailableValues = new List<string> { "0", "1" } },
-                new IniOverride { Key = "sightValue", Value = "14", Section = "PERFORMANCE", DisplayAs = TextBoxColumnName },
-                new IniOverride { Key = "modelDetail", Value = "1", Section = "PERFORMANCE", DisplayAs = TextBoxColumnName },
+                new IniOverride { Key = "recalc", Value = "0", Section = "PERFORMANCE", DisplayAs = DataGridColumnType.ComboBox, AvailableValues = new List<string> { "0", "1" } },
+                new IniOverride { Key = "sightValue", Value = "14", Section = "PERFORMANCE", DisplayAs = DataGridColumnType.TextBox },
+                new IniOverride { Key = "modelDetail", Value = "1", Section = "PERFORMANCE", DisplayAs = DataGridColumnType.TextBox },
             };
     }
 }
