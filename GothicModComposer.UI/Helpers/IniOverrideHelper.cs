aa10ccc4237e5a7c2cab417c80d3ecb8a1d522f6
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-07-19 09:55:26 +0200

    Added new ini overrides to the template from GAME section group.

--- GothicModComposer.UI/Helpers/IniOverrideHelper.cs
+++ GothicModComposer.UI/Helpers/IniOverrideHelper.cs
@@ -11,6 +11,9 @@ namespace GothicModComposer.UI.Helpers
                 new IniOverride { Key = "recalc", Value = "0", Section = "PERFORMANCE", DisplayAs = DataGridColumnType.ComboBox, AvailableValues = new List<string> { "0", "1" } },
                 new IniOverride { Key = "sightValue", Value = "14", Section = "PERFORMANCE", DisplayAs = DataGridColumnType.TextBox },
                 new IniOverride { Key = "modelDetail", Value = "1", Section = "PERFORMANCE", DisplayAs = DataGridColumnType.TextBox },
+                
+                new IniOverride { Key = "animatedWindows", Value = "1", Section = "GAME", DisplayAs = DataGridColumnType.ComboBox, AvailableValues = new List<string> { "0", "1" } },
+                new IniOverride { Key = "camLookaroundInverse", Value = "1", Section = "GAME", DisplayAs = DataGridColumnType.TextBox },
             };
     }
 }
