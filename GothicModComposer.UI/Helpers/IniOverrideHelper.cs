b1c76700583a16256e855baaf91c5a293fbf15f7
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-07-27 08:11:14 +0200

    Added some of the ini overrides from GAME section

--- GothicModComposer.UI/Helpers/IniOverrideHelper.cs
+++ GothicModComposer.UI/Helpers/IniOverrideHelper.cs
@@ -13,7 +13,18 @@ namespace GothicModComposer.UI.Helpers
                 new IniOverride { Key = "modelDetail", Value = "1", Section = "PERFORMANCE", DisplayAs = DataGridColumnType.TextBox },
                 
                 new IniOverride { Key = "animatedWindows", Value = "1", Section = "GAME", DisplayAs = DataGridColumnType.ComboBox, AvailableValues = new List<string> { "0", "1" } },
-                new IniOverride { Key = "camLookaroundInverse", Value = "1", Section = "GAME", DisplayAs = DataGridColumnType.TextBox },
+                new IniOverride { Key = "camLookaroundInverse", Value = "0", Section = "GAME", DisplayAs = DataGridColumnType.TextBox },
+                new IniOverride { Key = "bloodDetail", Value = "2", Section = "GAME", DisplayAs = DataGridColumnType.ComboBox, AvailableValues = new List<string> { "0", "1", "2", "3" } },
+                new IniOverride { Key = "extendedVideoKeys", Value = "0", Section = "GAME", DisplayAs = DataGridColumnType.TextBox },
+                new IniOverride { Key = "scaleVideos", Value = "0", Section = "GAME", DisplayAs = DataGridColumnType.ComboBox, AvailableValues = new List<string> { "0", "1" } },
+                new IniOverride { Key = "cameraLightRange", Value = "0", Section = "GAME", DisplayAs = DataGridColumnType.TextBox },
+                new IniOverride { Key = "invShowArrows", Value = "1", Section = "GAME", DisplayAs = DataGridColumnType.TextBox },
+                new IniOverride { Key = "invSwitchToFirstCategory", Value = "0", Section = "GAME", DisplayAs = DataGridColumnType.TextBox },
+                new IniOverride { Key = "enableMouse", Value = "1", Section = "GAME", DisplayAs = DataGridColumnType.ComboBox, AvailableValues = new List<string> { "0", "1" } },
+                new IniOverride { Key = "mouseSensitivity", Value = "0.5", Section = "GAME", DisplayAs = DataGridColumnType.TextBox },
+                new IniOverride { Key = "playLogoVideos", Value = "1", Section = "GAME", DisplayAs = DataGridColumnType.ComboBox, AvailableValues = new List<string> { "0", "1" } },
+                new IniOverride { Key = "skyEffects", Value = "1", Section = "GAME", DisplayAs = DataGridColumnType.ComboBox, AvailableValues = new List<string> { "0", "1" } },
+                new IniOverride { Key = "highlightMeleeFocus", Value = "2", Section = "GAME", DisplayAs = DataGridColumnType.TextBox },
             };
     }
 }
