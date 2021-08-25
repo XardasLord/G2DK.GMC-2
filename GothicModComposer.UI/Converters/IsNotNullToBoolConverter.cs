dbf96ce1e436d3222260b2e8331a3d58aa799100
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-08-25 07:43:52 +0200

    Disable RunMod/Update/Compose buttons when there is no 3D world selected

---
+++ GothicModComposer.UI/Converters/IsNotNullToBoolConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace GothicModComposer.UI.Converters
+{
+	public class IsNotNullToBoolConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) 
+			=> value != null;
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) 
+			=> throw new NotSupportedException("Two-way binding not supported by IsNotNullToBoolConverter");
+	}
+}
