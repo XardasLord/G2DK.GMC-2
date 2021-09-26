eec24c6473f866fc4efade45b4f056385a2a4825
Mateusz Stolecki <mateusz.stolecki@outlook.com>
2021-09-26 19:07:35 +0200

    Improved registry access stability

--- GothicModComposer.UI/Views/GmcSettings.xaml.cs
+++ GothicModComposer.UI/Views/GmcSettings.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Data;
 using GothicModComposer.UI.Models;
 using GothicModComposer.UI.ViewModels;
@@ -25,14 +26,30 @@ namespace GothicModComposer.UI.Views
 
         private void WindowsStartup_Checked(object sender, System.Windows.RoutedEventArgs e)
         {
-            var registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-            registryKey.SetValue("GMC-2 UI", System.Reflection.Assembly.GetEntryAssembly().Location);
+            try
+            {
+                var registryKey =
+                    Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                registryKey?.SetValue("GMC_2_UI", System.Reflection.Assembly.GetEntryAssembly().Location);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error during adding application to Windows startup.");
+            }
         }
 
         private void WindowsStartup_Unchecked(object sender, System.Windows.RoutedEventArgs e)
         {
-            var registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-            registryKey.DeleteValue("GMC-2 UI");
+            try
+            {
+                var registryKey =
+                    Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                registryKey?.DeleteValue("GMC_2_UI");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error during removing application from Windows startup");
+            }
         }
     }
 }
