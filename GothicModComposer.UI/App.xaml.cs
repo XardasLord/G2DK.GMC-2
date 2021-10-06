dca69a91639e4094652383d5e77ed7f9ff8e22b1
Mateusz Stolecki <mateusz.stolecki@outlook.com>
2021-10-06 20:15:22 +0200

    Added new actions for tray icon

--- GothicModComposer.UI/App.xaml.cs
+++ GothicModComposer.UI/App.xaml.cs
@@ -17,8 +17,9 @@ namespace GothicModComposer.UI
     {
         private readonly ServiceProvider _serviceProvider;
         private readonly NotifyIcon _notifyIcon;
+        private GmcVM _gmcVM;
         private MainWindow _mainWindow;
-        
+
         public App()
         {
             _notifyIcon = new NotifyIcon();
@@ -39,7 +40,7 @@ namespace GothicModComposer.UI
 
             services.AddSingleton<GmcVM>();
             services.AddSingleton<GmcSettingsVM>();
-            
+
             services.AddSingleton<MainWindow>();
         }
 
@@ -52,28 +53,45 @@ namespace GothicModComposer.UI
 
             _mainWindow.DataContext = _serviceProvider.GetService<GmcVM>();
             _mainWindow.Show();
-            
+
+            ConfigureTrayIcon();
+
+            _mainWindow.StateChanged += MainWindowOnStateChanged;
+        }
+
+        private void ConfigureTrayIcon()
+        {
             _notifyIcon.Icon = new Icon("Resources/GMC-logo.ico");
             _notifyIcon.Text = "GMC UI";
             _notifyIcon.Click += NotifyIconOnClick;
             _notifyIcon.ContextMenuStrip = new ContextMenuStrip();
-            _notifyIcon.ContextMenuStrip.Items.Add("Exit", null, (s, e) => Application.Current.Shutdown());
+            _gmcVM = _serviceProvider.GetRequiredService<GmcVM>();
+            _notifyIcon.ContextMenuStrip.Items.Add("Run mod", null, (s, _) => _gmcVM.RunModProfile.Execute(null));
+            _notifyIcon.ContextMenuStrip.Items.Add("Update", null, (s, _) => _gmcVM.RunUpdateProfile.Execute(null));
+            _notifyIcon.ContextMenuStrip.Items.Add("Compose", null, (s, _) => _gmcVM.RunComposeProfile.Execute(null));
+            _notifyIcon.ContextMenuStrip.Items.Add("Build mod file", null,
+                (s, _) => _gmcVM.RunBuildModFileProfile.Execute(null));
+            _notifyIcon.ContextMenuStrip.Items.Add("Restore Gothic", null,
+                (s, _) => _gmcVM.RunRestoreGothicProfile.Execute(null));
+            _notifyIcon.ContextMenuStrip.Items.Add("Exit", null, (s, _) => Current.Shutdown());
             _notifyIcon.Visible = true;
-            
-            _mainWindow.StateChanged += MainWindowOnStateChanged;
         }
 
-        private void MainWindowOnStateChanged(object? sender, EventArgs e)
+        private void MainWindowOnStateChanged(object sender, EventArgs e)
         {
             if (_mainWindow.WindowState == WindowState.Minimized)
                 _mainWindow.Hide();
         }
 
-        private void NotifyIconOnClick(object? sender, EventArgs e)
+        private void NotifyIconOnClick(object sender, EventArgs e)
         {
-            _mainWindow.Show();
-            _mainWindow.WindowState = WindowState.Normal;
-            _mainWindow.Activate();
+            var @event = e as MouseEventArgs;
+            if (@event?.Button == MouseButtons.Left)
+            {
+                _mainWindow.Show();
+                _mainWindow.WindowState = WindowState.Normal;
+                _mainWindow.Activate();
+            }
         }
 
         protected override void OnExit(ExitEventArgs e)
