66f0738b84cbb3d1c70ae3caec18ef843015c6ae
zuku05 <>
2021-10-12 20:41:14 +0200

    Bring Submods changes to oother branch
    remove submods change on this branch
    comment futre features ui elements

--- GothicModComposer.UI/MainWindow.xaml.cs
+++ GothicModComposer.UI/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Reflection;
 using GothicModComposer.UI.Services;
-using GothicModComposer.UI.Helpers;
 
 namespace GothicModComposer.UI
 {
@@ -29,18 +28,6 @@ namespace GothicModComposer.UI
         {
             var fullVersion = Assembly.GetExecutingAssembly().GetName().Version;
             Title = $"GMC UI v{fullVersion?.Major}.{fullVersion?.Minor}.{fullVersion?.Build}";
-        }
-
-        private void Test_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            SubmodsHelper submodsHelper = new SubmodsHelper();
-            submodsHelper.Main();
-            
-            TestText.Content+= submodsHelper.submods[0].Title
-            + "\n" + submodsHelper.submods[0].Version 
-            + "\n" + submodsHelper.submods[0].Authors[0]
-            + "\n" + submodsHelper.submods[0].Webpage
-            + "\n" + submodsHelper.submods[0].Description;
         }
     }
 }
