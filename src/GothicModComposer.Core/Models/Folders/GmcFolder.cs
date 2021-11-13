0d8945810454e8feeb17f208892ed319507741e5
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-11-13 21:37:32 +0100

    Got rid of unused class

--- src/GothicModComposer.Core/Models/Folders/GmcFolder.cs
+++ src/GothicModComposer.Core/Models/Folders/GmcFolder.cs
@@ -9,7 +9,6 @@ using GothicModComposer.Core.Models.Interfaces;
 using GothicModComposer.Core.Models.ModFiles;
 using GothicModComposer.Core.Presets;
 using GothicModComposer.Core.Utils.IOHelpers;
-using Microsoft.VisualBasic.FileIO;
 
 namespace GothicModComposer.Core.Models.Folders
 {
