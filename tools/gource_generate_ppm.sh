4eb3035440ce33dd2cd2435c1e95aa065e4ae8fc
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2021-11-26 22:18:48 +0100

    Added `Gource` visualization tool + readme

---
+++ tools/gource_generate_ppm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+gource \
+    -s .2 \
+    -1280x720 \
+    --auto-skip-seconds .1 \
+    --multi-sampling \
+    --stop-at-end \
+    --key \
+    --highlight-users \
+    --date-format "%d/%m/%y" \
+    --hide mouse,filenames \
+    --file-idle-time 0 \
+    --max-files 0  \
+    --background-colour 000000 \
+    --font-size 25 \
+    --output-ppm-stream output.ppm \
+    --output-framerate 30
